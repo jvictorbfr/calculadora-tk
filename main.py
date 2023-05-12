b72c1dd1ac2e8b1c965d88dba8b55d5886a9c875
João Victor <118749286+jvictorbfr@users.noreply.github.com>
2023-05-12 17:23:56 -0300

    Atualização de novas funções

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # LAYOUT DE LOGIN E SENHA by jvictor
 from tkinter import *
-from tkinter import Tk, ttk
+from tkinter import tix
 from tkinter import messagebox
 
 # FUNÇÕES--------------------------
-credenciais = ["jvictor", "botafogo"] # CREDENCIAL PADRÃO
+credenciais = ["jvictor", "botafogo"]  # CREDENCIAL PADRÃO
+
 # VERFICAR SENHA
 def verifica_senha():
     login = entry_login.get()
     senha = entry_senha.get()
 
     if login == "usuario" and senha == "01234":
-        messagebox.showinfo("Login", "Seja bem-vindo, usuário!")
+        # DELETA TELA 1
+        for widget in tela1.winfo_children():
+            widget.destroy()
+        # INICIA TELA DA CONTA
+        tela_userconta()
     elif credenciais[0] == login and credenciais[1] == senha:
         # DELETA TELA 1
         for widget in tela1.winfo_children():
@@ -23,8 +28,11 @@ def verifica_senha():
 
 # ESQUECEU SENHA
 def esqueceu_senha():
-    messagebox.showwarning("Atenção!", "LOGIN: usuario"
-                        "\nSENHA: 01234")
+    # DELETA TELA 1
+    for widget in tela1.winfo_children():
+        widget.destroy()
+    # INICIA TELA ESQUECEU SENHA
+    tela_esqsenha()
 
 # FUNÇÃO NOVA CONTA
 def nova_conta():
@@ -62,7 +70,7 @@ def tela_criarconta():
 
     # BOTÃO JÁ POSSUO CONTA
     bt_possuoconta = Button(tela1, text="JÁ POSSUO CONTA", bd=0, bg='#EE964B', activebackground='#EE964B',
-                         activeforeground='#252525', fg='#252525', font=('Ebrima', 7))
+                         activeforeground='#252525', fg='#252525', font=('Ebrima', 7), command=quit)
     bt_possuoconta.place(relx=0.35, rely=0.7)
 
     # BOTÃO CRIAR
@@ -70,7 +78,7 @@ def tela_criarconta():
                             fg='#EE964B', font=('Ebrima', 8, 'bold'), command=nova_conta)
     bt_criar.place(relx=0.2, rely=0.83, relwidth=0.6, relheight=0.083)
 
-# TELA ENTRAR CONTA
+# TELA ENTRAR CONTA JVICTOR
 def tela_entrarconta():
     # LABEL BEM-VINDO
     lb_bemvindo = Label(tela1, text="SEJA BEM-VINDO", bg='#EE964B', fg='#252525', font=('Ebrima', 15, 'bold'))
@@ -86,9 +94,64 @@ def tela_entrarconta():
     lb_nome.place(relx=0.1, rely=0.4)
 
     # LABEL LINK
-    lb_nome = Label(tela1, text="LINK: github.com/jvictorbfr",
+    lb_link = Label(tela1, text="LINK: github.com/jvictorbfr",
                     bg='#EE964B', fg='#252525', font=('Ebrima', 13, 'bold'))
-    lb_nome.place(relx=0.1, rely=0.6)
+    lb_link.place(relx=0.1, rely=0.6)
+
+    # BOTÃO SAIR
+    bt_sair = Button(tela1, text="SAIR", bd=2, bg='#252525', activeforeground='#EE964B',
+                            fg='#EE964B', font=('Ebrima', 8, 'bold'), command=quit)
+    bt_sair.place(relx=0.35, rely=0.83, relwidth=0.3, relheight=0.083)
+
+# TELA ENTRAR NA CONTA USUARIO
+def tela_userconta():
+    # LABEL BEM-VINDO
+    lb_bemvindo2 = Label(tela1, text="SEJA BEM-VINDO", bg='#EE964B', fg='#252525', font=('Ebrima', 15, 'bold'))
+    lb_bemvindo2.place(relx=0.2, rely=0.1)
+
+    # LABEL LINHA 2
+    lb_linha3 = Label(tela1, text="_____________________________________________________", bg='#EE964B', fg='#252525',
+                 font=('Ebrima', 9, 'bold'))
+    lb_linha3.place(relx=0.05, rely=0.25)
+
+    # LABEL NOME
+    lb_nome2 = Label(tela1, text="USER: usuario", bg='#EE964B', fg='#252525', font=('Ebrima', 13, 'bold'))
+    lb_nome2.place(relx=0.1, rely=0.4)
+
+    # LABEL LINK
+    lb_link2 = Label(tela1, text="LINK: github.com/jvictorbfr",
+                    bg='#EE964B', fg='#252525', font=('Ebrima', 13, 'bold'))
+    lb_link2.place(relx=0.1, rely=0.6)
+
+    # BOTÃO SAIR
+    bt_sair2 = Button(tela1, text="SAIR", bd=2, bg='#252525', activeforeground='#EE964B',
+                            fg='#EE964B', font=('Ebrima', 8, 'bold'), command=quit)
+    bt_sair2.place(relx=0.35, rely=0.83, relwidth=0.3, relheight=0.083)
+
+# TELA ESQUECEU A SENHA
+def tela_esqsenha():
+    # LABEL CREDENCIAIS DO USUARIO
+    lb_usercred = Label(tela1, text="CREDENCIAIS", bg='#EE964B', fg='#252525', font=('Ebrima', 15, 'bold'))
+    lb_usercred.place(relx=0.25, rely=0.1)
+
+    # LABEL LINHA
+    lb_linha4 = Label(tela1, text="_____________________________________________________", bg='#EE964B', fg='#252525',
+                 font=('Ebrima', 9, 'bold'))
+    lb_linha4.place(relx=0.05, rely=0.25)
+
+    # LABEL LOGIN USER
+    lb_userlog = Label(tela1, text="LOGIN: usuario", bg='#EE964B', fg='#252525', font=('Ebrima', 13, 'bold'))
+    lb_userlog.place(relx=0.1, rely=0.4)
+
+    # LABEL SENHA USER
+    lb_usersenha = Label(tela1, text="SENHA: 01234",
+                    bg='#EE964B', fg='#252525', font=('Ebrima', 13, 'bold'))
+    lb_usersenha.place(relx=0.1, rely=0.6)
+
+    # BOTÃO SAIR
+    bt_sair3 = Button(tela1, text="SAIR", bd=2, bg='#252525', activeforeground='#EE964B',
+                            fg='#EE964B', font=('Ebrima', 8, 'bold'), command=quit)
+    bt_sair3.place(relx=0.35, rely=0.83, relwidth=0.3, relheight=0.083)
 
 # MENU
 def menu():
@@ -102,7 +165,7 @@ def menu():
     filemenu.add_cascade(label="Sair", command=Quit)
 
 # JANELA---------------------------
-janela = Tk()
+janela = tix.Tk()
 janela.title("LOGIN E SENHA by Jvictor")
 janela.configure(background="#252525")
 janela.geometry("500x400")
@@ -139,14 +202,23 @@ bt_esqsenha = Button(tela1, text="ESQUECEU A SENHA?", bd=0, bg='#EE964B', active
                               activeforeground='#252525', fg='#252525', font=('Ebrima', 7), command=esqueceu_senha)
 bt_esqsenha.place(relx=0.1, rely=0.47)
 
+balao_esqsenha = tix.Balloon(tela1)
+balao_esqsenha.bind_widget(bt_esqsenha, balloonmsg="Esqueceu a senha")
+
 # BOTÃO ENTRAR
 bt_entrar = Button(tela1, text="ENTRAR", bd=2, bg='#252525', activeforeground='#EE964B',
                             fg='#EE964B', font=('Ebrima', 8, 'bold'), command=verifica_senha)
 bt_entrar.place(relx=0.35, rely=0.62, relwidth=0.3, relheight=0.083)
 
+balao_entrar = tix.Balloon(tela1)
+balao_entrar.bind_widget(bt_entrar, balloonmsg="Entrar na conta")
+
 # BOTÃO CRIAR CONTA
 bt_criar_conta = Button(tela1, text="NOVA CONTA", bd=2, bg='#F3F9E3', activebackground='#EE964B',
                     activeforeground='#F3F9E3', fg='#EE964B', font=('Ebrima', 8, 'bold'), command=nova_conta)
 bt_criar_conta.place(relx=0.2, rely=0.82, relwidth=0.6, relheight=0.083)
+
+balao_criarconta = tix.Balloon(tela1)
+balao_criarconta.bind_widget(bt_criar_conta, balloonmsg="Criar uma nova conta")
 
 janela.mainloop()
